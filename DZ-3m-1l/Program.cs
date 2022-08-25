f97a2c103b3daebb2fb190b6a10cba642fe6b718
meh <meh@IT5>
2022-08-25 09:48:16 +0800

    Style Change

--- DZ-3m-1l/Program.cs
+++ DZ-3m-1l/Program.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DZ_3m_1l
+﻿namespace DZ_3m_1l
 {
+    using System;
+    using System.Collections.Generic;
+
     internal class Program
     {
-        //Счётчик третьего метода
-        public static int counterV = 0;
-        public static int counterM = 0;
-        public static int counterE = 0;
-        public static int counter3 = 0;
+        // Счётчик третьего метода
+        private static int counterV = 0;
+        private static int counterM = 0;
+        private static int counterE = 0;
+        private static int counter3 = 0;
 
-        //Имена планет. к третьей программе пришёл к переменным. Устал писать каждое название.
-        public static string e = "Земля";
-        public static string v = "Венера";
+        // Имена планет. к третьей программе пришёл к переменным. Устал писать каждое название.
+        public static string E = "Земля";
+        public static string V = "Венера";
         public static string l = "Лимония";
         public static string m = "Марс";
 
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             AnonPlanet();
             EasyClassPlanet();
             AnotherMethod();
         }
 
-        static void AnonPlanet()
+        private static void AnonPlanet()
         {
-            //Создание четырёх анонимных объектов/планет
+            // Создание четырёх анонимных объектов/планет
             var venus = new
             {
                 Name = "Венера",
                 SolarNumber = 2,
                 EquatorLen = 12_104,
-                PreviousPlanet = (string)null
+                PreviousPlanet = (string)null,
             };
 
             var earth = new
@@ -41,7 +41,7 @@ namespace DZ_3m_1l
                 Name = "Земля",
                 SolarNumber = 3,
                 EquatorLen = 40_075,
-                PreviousPlanet = venus
+                PreviousPlanet = venus,
             };
 
             var mars = new
@@ -49,7 +49,7 @@ namespace DZ_3m_1l
                 Name = "Марс",
                 SolarNumber = 4,
                 EquatorLen = 21_344,
-                PreviousPlanet = earth
+                PreviousPlanet = earth,
             };
 
             var venus2 = new
@@ -57,33 +57,33 @@ namespace DZ_3m_1l
                 Name = "Венера",
                 SolarNumber = 2,
                 EquatorLen = 12_104,
-                PreviousPlanet = mars
+                PreviousPlanet = mars,
             };
 
-            //Вывод на консоль информации
-            //Console.WriteLine($"Название {venus.Name} " +
+            // Вывод на консоль информации
+            // Console.WriteLine($"Название {venus.Name} " +
             //    $"\n Порядковый номер от Солнца {venus.SolarNumber} " +
             //    $"\n Длина экватора  {venus.EquatorLen} " +
             //    $"\n Предыдущая планета {venus.PreviousPlanet}");
-            //Console.WriteLine("Венера = Венера - " + (venus.Equals(venus)));
+            // Console.WriteLine("Венера = Венера - " + (venus.Equals(venus)));
 
-            //Console.WriteLine($"Название {earth.Name} " +
+            // Console.WriteLine($"Название {earth.Name} " +
             //    $"\n Порядковый номер от Солнца {earth.SolarNumber} " +
             //    $"\n Длина экватора  {earth.EquatorLen} " +
             //    $"\n Предыдущая планета {earth.PreviousPlanet.Name}");
-            //Console.WriteLine("Земля = Венера - " + (earth.Equals(venus)));
+            // Console.WriteLine("Земля = Венера - " + (earth.Equals(venus)));
 
-            //Console.WriteLine($"Название {mars.Name} " +
+            // Console.WriteLine($"Название {mars.Name} " +
             //    $"\n Порядковый номер от Солнца {mars.SolarNumber} " +
             //    $"\n Длина экватора  {mars.EquatorLen} " +
             //    $"\n Предыдущая планета {mars.PreviousPlanet.Name}");
-            //Console.WriteLine("Марс = Венера - " + (mars.Equals(venus)));
+            // Console.WriteLine("Марс = Венера - " + (mars.Equals(venus)));
 
-            //Console.WriteLine($"Название {venus2.Name} " +
+            // Console.WriteLine($"Название {venus2.Name} " +
             //    $"\n Порядковый номер от Солнца {venus2.SolarNumber} " +
             //    $"\n Длина экватора  {venus2.EquatorLen} " +
             //    $"\n Предыдущая планета {venus2.PreviousPlanet.Name}");
-            //Console.WriteLine("Венера2 = Венера - " + (venus2.Equals(venus)));
+            // Console.WriteLine("Венера2 = Венера - " + (venus2.Equals(venus)));
 
             dynamic s = venus2;
             var nextStep = true;
@@ -95,7 +95,7 @@ namespace DZ_3m_1l
                 $"\n Порядковый номер от Солнца {s.SolarNumber} " +
                 $"\n Длина экватора  {s.EquatorLen} " +
                 $"\n Предыдущая планета {s.PreviousPlanet.Name}");
-                    Console.WriteLine($"{s.Name} = Венера - " + (s.Equals(venus)));
+                    Console.WriteLine($"{s.Name} = Венера - " + s.Equals(venus));
                     s = s.PreviousPlanet;
                 }
                 else
@@ -104,58 +104,61 @@ namespace DZ_3m_1l
                 $"\n Порядковый номер от Солнца {s.SolarNumber} " +
                 $"\n Длина экватора  {s.EquatorLen} " +
                 $"\n Предыдущая планета {s.PreviousPlanet}");
-                    Console.WriteLine($"{s.Name} = Венера - " + (s.Equals(venus)));
+                    Console.WriteLine($"{s.Name} = Венера - " + s.Equals(venus));
                     nextStep = false;
                 }
 
             } while (nextStep);
         }
 
-        //Метод вызова второй программы
-        static void EasyClassPlanet()
+        // Метод вызова второй программы
+        private static void EasyClassPlanet()
         {
             var catalogP = new CatalogPlanet();
 
-            Console.WriteLine(catalogP.GetPlanet(e));
-            Console.WriteLine(catalogP.GetPlanet(v));
+            Console.WriteLine(catalogP.GetPlanet(E));
+            Console.WriteLine(catalogP.GetPlanet(V));
             Console.WriteLine(catalogP.GetPlanet(l));
         }
 
-        //Метод вызова третьей программы
-        static void AnotherMethod()
+        // Метод вызова третьей программы
+        private static void AnotherMethod()
         {
             var catalogP = new CatalogPlanetA();
 
-            Console.WriteLine(catalogP.GetPlanet(e, catalogP.Validator));
-            Console.WriteLine(catalogP.GetPlanet(v, catalogP.Validator));
+            Console.WriteLine(catalogP.GetPlanet(E, catalogP.Validator));
+            Console.WriteLine(catalogP.GetPlanet(V, catalogP.Validator));
             Console.WriteLine(catalogP.GetPlanet(l, catalogP.Validator));
             Console.WriteLine(catalogP.GetPlanet(m, catalogP.Validator));
-            Console.WriteLine(catalogP.GetPlanet(e, catalogP.Validator));
-            Console.WriteLine(catalogP.GetPlanet(v, catalogP.Validator));
+            Console.WriteLine(catalogP.GetPlanet(E, catalogP.Validator));
+            Console.WriteLine(catalogP.GetPlanet(V, catalogP.Validator));
             Console.WriteLine(catalogP.GetPlanet(l, catalogP.Validator));
             Console.WriteLine(catalogP.GetPlanet(m, catalogP.Validator));
-            Console.WriteLine(catalogP.GetPlanet(e, catalogP.Validator));
-            Console.WriteLine(catalogP.GetPlanet(v, catalogP.Validator));
+            Console.WriteLine(catalogP.GetPlanet(E, catalogP.Validator));
+            Console.WriteLine(catalogP.GetPlanet(V, catalogP.Validator));
             Console.WriteLine(catalogP.GetPlanet(l, catalogP.Validator));
             Console.WriteLine(catalogP.GetPlanet(m, catalogP.Validator));
             Console.WriteLine(catalogP.GetPlanet("Другая планета", catalogP.Validator));
-            Console.WriteLine(catalogP.GetPlanet(e, message => counter3 == 3 ? " - Вы спрашиваете слишком часто" : null));
-            Console.WriteLine(catalogP.GetPlanet(e, message => counter3 == 3 ? " - Вы спрашиваете слишком часто" : null));
+            Console.WriteLine(catalogP.GetPlanet(E, message => counter3 == 3 ? " - Вы спрашиваете слишком часто" : null));
+            Console.WriteLine(catalogP.GetPlanet(E, message => counter3 == 3 ? " - Вы спрашиваете слишком часто" : null));
             Console.WriteLine(catalogP.GetPlanet(l, message => l == l ? " - Это запретная планета" : null));
-            Console.WriteLine(catalogP.GetPlanet(e, message => e == l ? " - Это запретная планета" : null));
-            Console.WriteLine(catalogP.GetPlanet(v, message => v == l ? " - Это запретная планета" : null));
-
+            Console.WriteLine(catalogP.GetPlanet(E, message => E == l ? " - Это запретная планета" : null));
+            Console.WriteLine(catalogP.GetPlanet(V, message => V == l ? " - Это запретная планета" : null));
         }
-        //Класс планета и класс список
-        class Planet
+
+        // Класс планета и класс список
+        private class Planet
         {
             public string Name { get; set; }
-            public int SolarNumber { get; set; }
-            public int EquatorLen { get; set; }
-            public Planet PreviousPlanet { get; set; }
 
+            public int SolarNumber { get; set; }
+
+            public int EquatorLen { get; set; }
+
+            public Planet PreviousPlanet { get; set; }
         }
-        class CatalogPlanet
+
+        private class CatalogPlanet
         {
             private List<Planet> listPlanet = new List<Planet>();
 
@@ -183,9 +186,10 @@ namespace DZ_3m_1l
                 listPlanet.Add(mars);
             }
 
-            //Мотод получения номера планеты и длины экватора
-            //Счётчик метода GetPlanet
-            int counter = 0;
+            // Мотод получения номера планеты и длины экватора
+            // Счётчик метода GetPlanet
+            private int counter = 0;
+
             public (int s, int e, string E) GetPlanet(string planetName)
             {
                 Planet p = new Planet();
@@ -220,9 +224,9 @@ namespace DZ_3m_1l
         }
 
 
-        //Список с новым методом
+        // Список с новым методом
 
-        class CatalogPlanetA
+        private class CatalogPlanetA
         {
             private List<Planet> listPlanet = new List<Planet>();
 
@@ -250,7 +254,7 @@ namespace DZ_3m_1l
                 listPlanet.Add(mars);
             }
 
-            //Новый метод получения номера планеты и длины экватора
+            // Новый метод получения номера планеты и длины экватора
 
 
             public string Validator(string namePl)
